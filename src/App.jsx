7a6faf7eea943022b232769ea3040f2d4e128f5c
mrMoraess <matheusrodriguesde347@gmail.com>
2024-01-15 17:26:47 -0300

    Testes de conexção com o server

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,33 @@ import GetStart from "./pages/GetStart.jsx";
 import {Routes, Route} from "react-router-dom"
 import Login from "./pages/LoginPage.jsx"
 import Regist from "./pages/RegistPage.jsx"
+import React, { useEffect, useState } from "react"
 
 function App() {
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data) 
+      }
+    )
+  }, [])
+
+  // const request = fetch(() => {
+  //   fetch("http://localhost:3000/useRegist").then(
+  //     response => response.json()
+  //   ).then(
+  //     data => {
+  //       console.log(data)
+  //     }
+  //   )
+  // })
+
+  // console.log(request)
+
   return (
     <>
       <Routes>
@@ -11,6 +36,13 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/regist" element={<Regist/>}/>
       </Routes>
+      {/* <div>
+        {(!backendData || !backendData.users) ? (
+          <p>Loading...</p>
+        ) : (
+          backendData.users
+        )}
+      </div> */}
     </>
   )
 }
