5975229acbf125f59c8a4022ee1225e0ceaeea58
mrMoraess <matheusrodriguesde347@gmail.com>
2024-01-17 17:05:29 -0300

    testes de requisição

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,47 @@ import Regist from "./pages/RegistPage.jsx"
 import React, { useEffect, useState } from "react"
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])
+  // const [backendData, setBackendData] = useState([{}])
 
-  useEffect(() => {
-    fetch("http://localhost:3000/").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data) 
-      }
-    )
-  }, [])
-
-  // const request = fetch(() => {
-  //   fetch("http://localhost:3000/useRegist").then(
+  // useEffect(() => {
+  //   fetch("http://localhost:3000").then(
   //     response => response.json()
   //   ).then(
   //     data => {
   //       console.log(data)
   //     }
   //   )
-  // })
+  // }, [])
 
-  // console.log(request)
+  // console.log(backendData.msg)
+
+  // const request = async () => {
+  //   const request = await fetch("http://localhost:3000")
+  //   return request.json()
+  // }
+
+  // (async () => {
+  //   try {
+  //     const data = await request()
+  //     console.log(data)
+  //   } catch (err) {
+  //     console.error(err)
+  //   }
+  // })()
+
+  fetch("http://localhost:3000/userRegist", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: "Matheus",
+      email: "matheusrodriguesde347@gmail.com",
+      password: "M3m3ntom0r1@"
+    })
+  }).then(
+    response => response.json()
+  ).then(data => { console.log(data) })
 
   return (
     <>
@@ -36,13 +54,6 @@ function App() {
         <Route path="/login" element={<Login/>}/>
         <Route path="/regist" element={<Regist/>}/>
       </Routes>
-      {/* <div>
-        {(!backendData || !backendData.users) ? (
-          <p>Loading...</p>
-        ) : (
-          backendData.users
-        )}
-      </div> */}
     </>
   )
 }
